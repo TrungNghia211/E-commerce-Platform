17251d0034744f1d48538fd934412a910e2e894b
Nghia Trung Nguyen <nghiatrung2112003@gmail.com>
2025-06-12 13:09:02 +0700

    CSS AdminHeader component

--- client/e-commerce/src/components/ui/AdminHeader/AdminHeader.tsx
+++ client/e-commerce/src/components/ui/AdminHeader/AdminHeader.tsx
@@ -1,3 +1,5 @@
+import { Avatar, Space } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import classNames from "classnames/bind";
 
 import styles from "./AdminHeader.module.scss";
@@ -5,5 +7,16 @@ import styles from "./AdminHeader.module.scss";
 const cx = classNames.bind(styles);
 
 export default function AdminHeader() {
-  return <div className={cx("container")}>Admin Header</div>;
+  return (
+    <div className={cx("container")}>
+      <div className={cx("logo")}>Admin Dashboard</div>
+
+      <div className={cx("user-section")}>
+        <Space className={cx("user-info")}>
+          <span className={cx("welcome-text")}>Chào mừng, Admin</span>
+          <Avatar size="small" icon={<UserOutlined />} />
+        </Space>
+      </div>
+    </div>
+  );
 }
